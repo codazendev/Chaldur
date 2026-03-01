63be1e9c164f1a4fcbc9aaef18c5df4fd1e84d2b
Jeremy King <codazendev@gmail.com>
2026-02-28 23:30:25 -0500

    add mod icon

--- chaldur.lua
+++ chaldur.lua
@@ -1,2 +1,12 @@
 -- Definitions
 Chaldur = SMODS.current_mod
+
+-- Chaldur event queue
+G.E_MANAGER.queues.chaldur = {}
+
+SMODS.Atlas({
+    key = 'modicon',
+    path = 'chaldur_icon.png',
+    px = 32,
+    py = 32
+})
